73b07535f1f8313bcb93efa5e5e427af5ec7d157
Tobias Martens <tm@whoelse.ai>
2019-07-21 21:35:19 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const app = function () {
 	const API_BASE = 'https://script.google.com/macros/s/AKfycbyJRxBWqz43ogRDC5sBDfze85y_r4QccXQ6ekapQqfoF5NAueB5/exec';
 	const API_KEY = 'abcdef';
-	const CATEGORIES = ['general', 'NLP', 'data science', 'business models', 'agile teams','innovation methods','videos'];
+	const CATEGORIES = [];
 
 	const state = {activePage: 1, activeCategory: null};
 	const page = {};
