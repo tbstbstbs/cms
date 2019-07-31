c40df380feeda2e9a6e7e714653bad4cc8b919d6
Tobias Martens <tm@whoelse.ai>
2019-07-31 20:34:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const app = function () {
 	const API_BASE = 'https://script.google.com/macros/s/AKfycbyJRxBWqz43ogRDC5sBDfze85y_r4QccXQ6ekapQqfoF5NAueB5/exec';
 	const API_KEY = 'abcdef';
-	const CATEGORIES = ['NLP','Blockchain','Smart City', 'Drones', 'Agile', 'Study Programs', 'Arts', 'UX','Design Thinking'];
+	const CATEGORIES = ['NLP','Blockchain','Smart City'];
 
 	const state = {activePage: 1, activeCategory: null};
 	const page = {};
