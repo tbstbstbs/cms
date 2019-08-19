09dead8b823451168d724a8377c7666e93578919
Tobias Martens <tm@whoelse.ai>
2019-08-20 00:43:39 +0200

    Update backup.js

--- js/backup.js
+++ js/backup.js
@@ -1,4 +1,4 @@
 const app = function () {
 	const API_BASE = 'https://script.google.com/macros/s/AKfycbyJRxBWqz43ogRDC5sBDfze85y_r4QccXQ6ekapQqfoF5NAueB5/exec';
 	const API_KEY = 'abcdef';
-	const CATEGORIES = ['general', 'NLP', 'data science', 'business models', 'agile teams','innovation methods','videos'];
+	const CATEGORIES = ['general', 'NLP', 'datasci', 'business models', 'agile','innovation methods','videos'];
